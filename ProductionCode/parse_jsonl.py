528dd9f5030f452187179025c064e2b8bada7187
Devin Gulliver <gulliverd@carleton.edu>
2025-04-20 15:56:55 -0500

    Add doctrings, fix typo

--- ProductionCode/parse_jsonl.py
+++ ProductionCode/parse_jsonl.py
@@ -1,9 +1,12 @@
 import json
 
 def parse_goodreads_jsonl(filename):
-    # create list to add objects to
+    """Parse the Goodreads data, which is in JSONL format.
+    Arguments:
+    filename - the path to the JSONL (one JSON per line) file.
+    Returns: a list of objects parsed from the JSONL file.
+    """
     data = []
-    # open the file, iterate over lines
     with open(filename, "r") as file:
         for line in file:
             # for each line, parse it and add it to the list
@@ -11,7 +14,8 @@ def parse_goodreads_jsonl(filename):
     return data
 
 def main():
-    goodreads_data = parse_goodreads_jsonl("Data/goodreads_subset_k.jsonl") # to replace with full db eventually
+    """Main function for informal testing."""
+    goodreads_data = parse_goodreads_jsonl("Data/goodreads_subset_k.jsonl")
     print(goodreads_data[0])
 
 if __name__  == "__main__":
